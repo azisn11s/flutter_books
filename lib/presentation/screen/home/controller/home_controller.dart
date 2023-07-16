c85f7666d033233665ae44a78db8996f043f72ba
azisnovian.kom48 <azisnovian.kom48@gmail.com>
2023-07-16 11:12:14 +0700

    Tugas 4: list books with title & subtitle

--- lib/presentation/screen/home/controller/home_controller.dart
+++ lib/presentation/screen/home/controller/home_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_app/data/model/book_model.dart';
 import 'package:flutter_app/domain/use_case/get_books_use_case.dart';
+import 'package:flutter_app/presentation/state/ViewState.dart';
 import 'package:get/get.dart';
 
 class HomeController extends GetxController {
@@ -8,10 +9,15 @@ class HomeController extends GetxController {
   HomeController({required this.getBooksUseCase});
 
   Rx<List<Book>> listBook = Rx([]);
+  Rx<ViewState> homeState = Rx(ViewState.initial);
 
   void getBooks() async {
+    homeState(ViewState.loading);
+    update();
+
     final listBooks = await getBooksUseCase.call();
     listBook(listBooks);
+    homeState(ViewState.success);
     update();
   }
 }
--- lib/presentation/screen/home/view/home_view.dart
+++ lib/presentation/screen/home/view/home_view.dart
@@ -4,6 +4,7 @@ import 'package:flutter_app/data/data_source/remote/book_remote.dart';
 import 'package:flutter_app/data/repository/book_repository_implementation.dart';
 import 'package:flutter_app/domain/use_case/get_books_use_case.dart';
 import 'package:flutter_app/presentation/screen/home/controller/home_controller.dart';
+import 'package:flutter_app/presentation/state/ViewState.dart';
 import 'package:get/get.dart';
 
 class HomeView extends StatelessWidget {
@@ -24,11 +25,26 @@ class HomeView extends StatelessWidget {
           init: homeController,
           initState: (state) => homeController.getBooks(),
           builder: (state) {
+            if (state.homeState.value == ViewState.loading) {
+              return const Center(child: CircularProgressIndicator());
+            }
             return ListView.builder(
                 itemCount: state.listBook.value.length,
                 shrinkWrap: true,
                 itemBuilder: (context, index) {
-                  return Text(state.listBook.value[index].title);
+                  return Card(
+                      child: Padding(
+                          padding: const EdgeInsets.all(9.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                state.listBook.value[index].title,
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text(state.listBook.value[index].subtitle),
+                            ],
+                          )));
                 });
           }),
     );
